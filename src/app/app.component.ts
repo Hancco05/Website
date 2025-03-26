1e9592790ed7a494a49f4b203eef7962b01d7386
hancco05 <s.carmonawright@gmail.com>
2025-03-25 21:47:10 -0300

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./componets/navbar/navbar.component";
+import { MainComponent } from "./componets/main/main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [NavbarComponent, MainComponent]
 })
 export class AppComponent {
   title = 'website';
 }
+
