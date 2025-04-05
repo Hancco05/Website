0f2cacced3563bf23df2c81faf9f4f06424ff96f
hancco05 <s.carmonawright@gmail.com>
2025-04-04 22:04:42 -0300

    commit 4

--- src/app/componets/tiempo/tiempo.component.ts
+++ src/app/componets/tiempo/tiempo.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // útil para directivas básicas como *ngIf
 
 @Component({
   selector: 'app-tiempo',
-  imports: [],
+  standalone: true, // ← MUY importante si estás usando standalone components
+  imports: [CommonModule, ReactiveFormsModule], // ← Aquí importa ReactiveFormsModule
   templateUrl: './tiempo.component.html',
-  styleUrl: './tiempo.component.css'
+  styleUrls: ['./tiempo.component.css']
 })
-export class TiempoComponent {
+export class TiempoComponent implements OnInit {
+  formulario!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.iniciaFormulario();
+  }
+
+  ngOnInit(): void {}
+
+  iniciaFormulario(): void {
+    this.formulario = this.fb.group({
+      ciudad: ['Santiago'],
+      codigo: ['CL']
+    });
+  }
+
+  consultar(): void {
+    console.log(this.formulario.value);
+  }
 }
