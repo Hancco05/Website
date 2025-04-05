663a05e22cd2a3752dc1386d6b9ca71a6d0c356a
hancco05 <s.carmonawright@gmail.com>
2025-04-05 01:42:24 -0300

    commit 8

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TiempoComponent } from '../app/componets/tiempo/tiempo.component';
+import { MainComponent } from '../app/componets/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'tiempo', component: TiempoComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' }
+];
